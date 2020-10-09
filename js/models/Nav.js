9f0e3e6441e9a0a0175d07d27695b568e476d6c5
Francois Sommella <francois.sommella@gmail.com>
2020-10-09 10:34:39 +0200

    we

--- js/models/Nav.js
+++
@@ -1,8 +0,0 @@
-// import {elements} from './views/selectors.js';
-import * as navView from '../views/navView.js';
-
-
-export function burgerMenuToggle() {
-  console.log("click works");
-  navView.displayMenuToggle();
-}
